c1093d9a5744e8e08c0a28f7a9b84873da1cda21
Benno Rem <benno.rem@spec.de>
2024-03-22 13:51:25 +0100

    Improved driver version check for WRITING_TO_LOG register

--- src/spcm/__init__.py
+++ src/spcm/__init__.py
@@ -43,11 +43,16 @@ __version__ = _version.get_versions()['version']
 
 # Writing spcm package version to log file
 try:
-    from .pyspcm import spcm_dwSetParam_ptr, create_string_buffer
+    from .pyspcm import spcm_dwGetParam_i64, spcm_dwSetParam_ptr, create_string_buffer, byref, int64
+    driver_version = int64(0)
+    spcm_dwGetParam_i64(None, SPC_GETDRVVERSION, byref(driver_version))
+    # Available starting from build 21797
+    if (driver_version.value & 0x0000FFFF) < 21797:
+        raise OSError("Driver version {} does not support writing spcm version to log".format(driver_version))
     version_str = bytes("Python package spcm v{}".format(__version__), "utf-8")
     version_ptr = create_string_buffer(version_str)
     dwErr = spcm_dwSetParam_ptr(None, SPC_WRITE_TO_LOG, version_ptr, len(version_str))
 except OSError as e:
-    print("Writing spcm version to log not yet supported")
+    print(e)
 
 #print(__version__)
